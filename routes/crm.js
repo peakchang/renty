be8f9014c16ea92117aca6db4ef86393dd4764f5
parkchangyong <ridebus82@nate.com>
2022-09-24 18:44:03 +0900

    0924 asjhdgwjrtlgjqerweth

--- routes/crm.js
+++ routes/crm.js
@@ -8,11 +8,11 @@ const router = express.Router();
 
 router.get('/', async (req, res, next) => {
     console.log('아아니 씨발 넌 또 왜 지랄인데');
-    const allDbSql = "SELECT * FROM application_form;";
-
-    wData = await sql_con.promise().query(allDbSql)
-
-    res.render('crm/crm_main', {wData});
+    const allDbSql = "SELECT * FROM application_form WHERE mb_name='testname';";
+    const tempData = await sql_con.promise().query(allDbSql)
+    const wData = tempData[0];
+    console.log(wData);
+    res.render('crm/crm_main', { wData });
 })
 
 module.exports = router;
